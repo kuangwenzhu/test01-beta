1a1d742821915f257db1c22436c4aec18f4713ba
paas <paas@cse-codegen-backend-3223585361-ngb79>
2018-01-09 08:06:21 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/zcstest/controller/ZcstestDelegate.java
@@ -0,0 +1,14 @@
+package com.service.zcstest.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ZcstestDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
